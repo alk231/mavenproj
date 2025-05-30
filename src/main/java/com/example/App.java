1eed5268a6d47b9f8bfab3c55fec22fe11414238
alok kumar <123632977+alk231@users.noreply.github.com>
2025-05-30 13:06:52 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         int a=1,b=2;
-        int sum=a+b;
-        System.out.println("sum="+sum);
+        int sum1=a+b;
+        System.out.println("sum="+sum1);
     }
 }
