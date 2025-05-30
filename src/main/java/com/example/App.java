e48528ec9ba515967b0e2f03a78a8fc8e93c8528
alk231 <kumaralokpvt@gmail.com>
2025-05-30 12:23:47 +0530

    one

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a=1,b=2;
+        int sum=a+b;
+        System.out.println("sum="+sum);
+    }
+}
